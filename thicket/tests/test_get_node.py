29195595f7bc31f4cae5428a216653049a3c87e3
Michael McKinsey <michaelmckinsey@tamu.edu>
2025-02-13 16:02:45 -0800

    Add options to get_node to specify which node to return if multiple (#229)

--- thicket/tests/test_get_node.py
+++ thicket/tests/test_get_node.py
@@ -9,12 +9,20 @@ import pytest
 def test_get_node(literal_thickets):
     tk, _, _ = literal_thickets
 
+    # Check error raised
     with pytest.raises(KeyError):
         tk.get_node("Foo")
 
-    baz = tk.get_node("Baz")
+    # Check case which="first"
+    qux1 = tk.get_node("Qux", which="first")
+    assert qux1.frame["name"] == "Qux"
+    assert qux1._hatchet_nid == 1
 
-    # Check node properties
-    assert baz.frame["name"] == "Baz"
-    assert baz.frame["type"] == "function"
-    assert baz._hatchet_nid == 0
+    # Check case which="last"
+    qux2 = tk.get_node("Qux", which="last")
+    assert qux2.frame["name"] == "Qux"
+    assert qux2._hatchet_nid == 2
+
+    # Check case which="all"
+    qux_all = tk.get_node("Qux", which="all")
+    assert len(qux_all) == 2
